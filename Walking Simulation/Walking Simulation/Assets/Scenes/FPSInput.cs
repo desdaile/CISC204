48c6b26c40a41e004df8f80a740cf8d495dd387d
Dontae Esdaile <33205190+desdaile@users.noreply.github.com>
2023-02-26 23:19:46 -0500

    Pretty Much done

---
+++ Walking Simulation/Walking Simulation/Assets/Scenes/FPSInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+    private CharacterController charController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        charController.Move(movement);
+        transform.Translate(deltaX * Time.deltaTime, 0, deltaZ * Time.deltaTime);
+    }
+}
